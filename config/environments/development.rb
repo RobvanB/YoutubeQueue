a9535233ad36f10d763f1a6bf71adc2e14d83c01
Rob vanBrandenburg <118380+RobvanB@users.noreply.github.com>
2026-05-16 06:43:46 -0400

    Fixes for Ruby 4 update

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,6 +3,12 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  # Added by RvB - For Ruby 4 update
+  config.web_console.whitelisted_ips = '192.168.0.0/16'
+
+
+  # End RvB
+	
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
   # since you don't have to restart the web server when you make code changes.
